fb2f49d0d9d1fd030ae198869a73c9a1f9542d80
aabccd021 <aabccd021@gmail.com>
2021-10-03 14:05:51 +0700

    example

---
+++ firebase/functions/src/example.ts
@@ -0,0 +1,50 @@
+// eslint-disable-next-line no-restricted-imports
+import * as admin from 'firebase-admin';
+import { makeMasmottTriggers } from './get-trigger';
+
+admin.initializeApp();
+
+export const triggers = makeMasmottTriggers(admin.firestore(), {
+  user: {
+    src: {
+      id: {
+        type: 'string',
+      },
+      username: {
+        type: 'string',
+      },
+    },
+    views: {
+      card: {
+        selectedFieldNames: ['username'],
+        join: [],
+      },
+    },
+  },
+  article: {
+    src: {
+      text: {
+        type: 'string',
+      },
+      owner: {
+        type: 'refId',
+        refCollection: 'user',
+      },
+    },
+    views: {
+      card: {
+        selectedFieldNames: ['text'],
+        join: [
+          {
+            firstRef: {
+              collectionName: 'user',
+              fieldName: 'owner',
+            },
+            refChain: [],
+            selectedFieldNames: ['username'],
+          },
+        ],
+      },
+    },
+  },
+});
