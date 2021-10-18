129d42f46f893e8fd86feb8984d9800929de41a1
aabccd021 <aabccd021@gmail.com>
2021-10-18 19:01:40 +0700

    werks

--- packages/server/test/view/count.test.ts
+++ packages/server/test/view/count.test.ts
@@ -4,25 +4,39 @@ import * as firestore from 'firebase-admin/firestore';
 import sinon, { stubInterface } from 'ts-sinon';
 import { getDoc } from '../../src/wrapper/firebase-admin';
 
-const snapshot = stubInterface<firestore.DocumentSnapshot>();
-
-const doc = stubInterface<firestore.DocumentReference>();
-doc.get.resolves(snapshot);
-
-const firestoreInstance = stubInterface<firestore.Firestore>();
-firestoreInstance.doc.returns(doc);
-
-const getFirestore = sinon
-  .stub(firestore, 'getFirestore')
-  .returns(firestoreInstance);
-
 describe('count view', () => {
-  it('on counted document created', () => {
+  it('on counted document created', async () => {
+    const snapshot: firestore.DocumentSnapshot = {
+      ...stubInterface<firestore.DocumentSnapshot>(),
+      id: 'hogeId',
+      data: () => ({
+        lorem: 'ipsum',
+      }),
+    };
+
+    const doc = stubInterface<firestore.DocumentReference>();
+    doc.get.resolves(snapshot);
+
+    const firestoreInstance = stubInterface<firestore.Firestore>();
+    firestoreInstance.doc.returns(doc);
+
+    const getFirestore = sinon
+      .stub(firestore, 'getFirestore')
+      .returns(firestoreInstance);
+
     const app1: App = { name: '', options: {} };
-    getDoc(app1, 'fooCollection', 'barId');
+
+    const wrappedSnapshot = await getDoc(app1, 'fooCollection', 'barId');
+
     expect(getFirestore.calledOnceWith(app1)).to.be.true;
     expect(firestoreInstance.doc.calledOnceWith('fooCollection/barId')).to.be
       .true;
     expect(doc.get.calledOnceWith()).to.be.true;
+    expect(wrappedSnapshot).to.deep.equal({
+      data: {
+        lorem: 'ipsum',
+      },
+      id: 'hogeId',
+    });
   });
 });
