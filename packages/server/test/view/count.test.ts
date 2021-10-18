071004cd9fa28179a4a012e1ecfbefa4e4096068
aabccd021 <aabccd021@gmail.com>
2021-10-18 18:18:04 +0700

    can test

--- packages/server/test/view/count.test.ts
+++ packages/server/test/view/count.test.ts
@@ -1,11 +1,17 @@
 import { expect } from 'chai';
 import { App } from 'firebase-admin/app';
-import { onCountedDocCreated } from '../../src/view/count';
+import * as firestore from 'firebase-admin/firestore';
+import sinon from 'ts-sinon';
+import { getDoc } from '../../src/wrapper/firebase-admin';
+
+const getFirestore = sinon.stub(firestore, 'getFirestore');
 
 describe('count view', () => {
   it('on counted document created', () => {
-    const app: App = { name: '', options: {} };
-    const triggers = onCountedDocCreated(app, 'a', 'b', {});
-    expect(triggers).to.deep.equal({});
+    const app1: App = { name: '', options: {} };
+    const app2: App = { name: 'b', options: {} };
+    getDoc(app1, 'a', 'b');
+    expect(getFirestore.calledOnceWith(app1)).to.be.true;
+    expect(getFirestore.calledOnceWith(app2)).to.be.false;
   });
 });
