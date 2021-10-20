7f813353ff4389e0d068b0541a5760c49ac97e10
aabccd021 <aabccd021@gmail.com>
2021-10-20 12:06:58 +0700

    fast-check

--- packages/server/test/firebase-admin/util.test.ts
+++ packages/server/test/firebase-admin/util.test.ts
@@ -1,4 +1,5 @@
 import { assert } from 'chai';
+import * as fc from 'fast-check';
 import { App } from 'firebase-admin/app';
 import * as firestore from 'firebase-admin/firestore';
 import sinon, { stubInterface } from 'ts-sinon';
@@ -10,33 +11,41 @@ describe('firebase-admin', () => {
 
   describe('getDocRef', () => {
     it('gets the document', async () => {
-      // arrange
-      const mockedDocRef = stubInterface<firestore.DocumentReference>();
+      fc.assert(
+        fc
+          .property(fc.string(), fc.string(), (collectionName, documentId) => {
+            // arrange
+            const mockedDocRef = stubInterface<firestore.DocumentReference>();
 
-      const mockedCollectionRef =
-        stubInterface<firestore.CollectionReference>();
-      mockedCollectionRef.doc.returns(mockedDocRef);
+            const mockedCollectionRef =
+              stubInterface<firestore.CollectionReference>();
+            mockedCollectionRef.doc.returns(mockedDocRef);
 
-      const mockedFirestore = stubInterface<firestore.Firestore>();
-      mockedFirestore.collection.returns(mockedCollectionRef);
+            const mockedFirestore = stubInterface<firestore.Firestore>();
+            mockedFirestore.collection.returns(mockedCollectionRef);
 
-      const getFirestore = sinon
-        .stub(firestore, 'getFirestore')
-        .returns(mockedFirestore);
+            const getFirestore = sinon
+              .stub(firestore, 'getFirestore')
+              .returns(mockedFirestore);
 
-      const app = stubInterface<App>();
-      const app2 = stubInterface<App>();
+            const app = stubInterface<App>();
+            const app2 = stubInterface<App>();
 
-      // act
-      const docRef = getDocRef(app, 'fooCollection', 'barId');
+            // act
+            const docRef = getDocRef(app, collectionName, documentId);
 
-      // assert
-      assert.isTrue(getFirestore.calledOnceWith(app));
-      assert.isFalse(getFirestore.calledOnceWith(app2));
+            // assert
+            assert.isTrue(getFirestore.calledOnceWith(app));
+            assert.isFalse(getFirestore.calledOnceWith(app2));
 
-      assert.isTrue(mockedFirestore.collection.calledOnceWith('fooCollection'));
-      assert.isTrue(mockedCollectionRef.doc.calledOnceWith('barId'));
-      assert.equal(docRef, mockedDocRef);
+            assert.isTrue(
+              mockedFirestore.collection.calledOnceWith(collectionName)
+            );
+            assert.isTrue(mockedCollectionRef.doc.calledOnceWith(documentId));
+            assert.equal(docRef, mockedDocRef);
+          })
+          .afterEach(() => sinon.restore())
+      );
     });
   });
 });
