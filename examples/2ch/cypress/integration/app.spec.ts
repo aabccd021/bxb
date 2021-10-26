2851298dba6f9bd92d450f9f59e3eeda7a94b4ff
aabccd021 <aabccd021@gmail.com>
2021-10-26 14:19:29 +0700

    success without retry

--- examples/2ch/cypress/integration/app.spec.ts
+++ examples/2ch/cypress/integration/app.spec.ts
@@ -1,28 +1,19 @@
 describe('Creation', function () {
-  it(
-    'should be able to create a thread',
-    {
-      retries: {
-        runMode: 1,
-        openMode: 1,
-      },
-    },
-    function () {
-      cy.visit('/');
+  it('should be able to create a thread', function () {
+    cy.visit('/');
 
-      cy.get('button').contains('Create').click();
+    cy.get('button').contains('Create').click();
 
-      cy.url().should('include', '/thread/');
-      cy.contains('Thread Id : ');
-      cy.contains('replyCount : 0');
+    cy.url().should('include', '/thread/');
+    cy.contains('Thread Id : ');
+    cy.contains('replyCount : 0');
 
-      // eslint-disable-next-line cypress/no-unnecessary-waiting
-      cy.wait(500);
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(500);
 
-      cy.get('input').type('aabccd').should('have.value', 'aabccd');
-      cy.get('button').contains('post').click();
+    cy.get('input').type('aabccd').should('have.value', 'aabccd');
+    cy.get('button').contains('post').click();
 
-      // cy.contains('replyCount : 1');
-    }
-  );
+    // cy.contains('replyCount : 1');
+  });
 });
