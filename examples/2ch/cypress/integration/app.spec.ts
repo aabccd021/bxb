4456cfa7ff14201461dd622ffa2e9ecec4601def
aabccd021 <aabccd021@gmail.com>
2021-10-26 09:05:32 +0700

    weks if waited

--- examples/2ch/cypress/integration/app.spec.ts
+++ examples/2ch/cypress/integration/app.spec.ts
@@ -3,8 +3,8 @@ describe('Creation', function () {
     'should be able to create a thread',
     {
       retries: {
-        runMode: 2,
-        openMode: 2,
+        runMode: 1,
+        openMode: 1,
       },
     },
     function () {
@@ -13,9 +13,11 @@ describe('Creation', function () {
       cy.get('button').contains('Create').click();
 
       cy.url().should('include', '/thread/');
-
       cy.contains('Thread Id : ');
       cy.contains('replyCount : 0');
+
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(500);
 
       cy.get('input').type('aabccd').should('have.value', 'aabccd');
       cy.get('button').contains('post').click();
