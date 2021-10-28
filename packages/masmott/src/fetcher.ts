fdc3faf53832496dca67f657c16794cb84d798b1
aabccd021 <aabccd021@gmail.com>
2021-10-28 17:30:30 +0700

    dynamic import

--- packages/masmott/src/fetcher.ts
+++ packages/masmott/src/fetcher.ts
@@ -1,10 +1,10 @@
-import { doc, getDoc, getFirestore } from 'firebase/firestore/lite';
 import { DocSnapshot } from './types';
 
 export async function fetcher(path: string): Promise<DocSnapshot> {
-  const firestore = getFirestore();
-  const docRef = doc(firestore, path);
-  const snapshot = await getDoc(docRef);
+  const firestoreModule = await import('firebase/firestore/lite');
+  const firestore = firestoreModule.getFirestore();
+  const docRef = firestoreModule.doc(firestore, path);
+  const snapshot = await firestoreModule.getDoc(docRef);
   const data = snapshot.data();
   if (data !== undefined) {
     return { exists: true, data };
