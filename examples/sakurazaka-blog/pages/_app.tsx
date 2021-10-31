92eefd23ccd03ac421a41d49bcce2531e8db9cea
aabccd021 <aabccd021@gmail.com>
2021-10-31 17:16:47 +0700

    delete examples

--- examples/sakurazaka-blog/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from "next/app";
-
-import { useMasmott } from "../hooks/init";
-
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  useMasmott();
-  return <Component {...pageProps} />;
-}
-export default MyApp;
