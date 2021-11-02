5bc06f614b24c5499c4576a4ee0412bb0d15158f
aabccd021 <aabccd021@gmail.com>
2021-11-02 08:31:36 +0700

    dunno

--- packages/masmott/cli/etc.ts
+++ packages/masmott/cli/etc.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 export const firebase = {
   emulators: {
     firestore: {
@@ -112,8 +113,7 @@ export const nextEnv = `
 // see https://nextjs.org/docs/basic-features/typescript for more information.
 `;
 
-export const nextCOnfig = `
-module.exports = {
+export const nextConfig = {
   reactStrictMode: true,
   redirects: () => {
     return [
@@ -122,10 +122,9 @@ module.exports = {
         destination: '/thread/new',
         permanent: false,
       },
-    ]
-  }
-}
-`;
+    ];
+  },
+};
 
 export const cypressPlugins = `
 module.exports = (on, config) => {
@@ -169,3 +168,90 @@ import Page from '../../web/thread/new';
 
 export default Page;`;
 }
+
+export function packageJson(): string {
+  const packageJsonContent = {
+    name: '2ch',
+    version: '0.1.0',
+    private: true,
+    engines: {
+      node: '14',
+    },
+    main: '.functions/index.js',
+    scripts: {
+      cli: 'node node_modules/masmott/bin/cli/index.js',
+      lint: 'eslint ./ --ext ts,tsx',
+      'clean-coverage': 'rm -rf .nyc_output && rm -rf coverage',
+      'server:build': 'node compiler.js',
+      'client:build': 'rm -rf .next && next build',
+      build: 'yarn client:build && yarn server:build',
+      'start:cached': 'firebase emulators:start',
+      start: 'yarn build && yarn start:cached',
+      'test:cached': 'yarn clean-coverage && firebase emulators:exec "cypress run"',
+      test: 'yarn build && yarn test:cached',
+      'test:gui:cached': 'yarn clean-coverage && firebase emulators:exec "cypress run --headed"',
+      'test:gui': 'yarn build && yarn test:gui:cached',
+      'test:open:cached': 'firebase emulators:exec "cypress open"',
+      'test:open': 'yarn build && yarn test:open:cached',
+      'server:dev:cached': 'firebase emulators:start',
+      'server:dev': 'yarn server:build && yarn server:dev:cached',
+      'client:dev': 'next dev',
+      analyze: 'cross-env ANALYZE=true next build',
+    },
+    dependencies: {
+      'babel-plugin-istanbul': '^6.1.1',
+      'firebase-admin': '^10.0.0',
+      'firebase-functions': '^3.15.7',
+      masmott: 'file:./../../packages/masmott',
+      next: '^12.0.1',
+      react: '17.0.2',
+      'react-dom': '^17.0.2',
+    },
+    devDependencies: {
+      '@cypress/code-coverage': '^3.9.11',
+      '@next/bundle-analyzer': '^12.0.1',
+      '@types/react': '17.0.32',
+      'cross-env': '^7.0.3',
+      cypress: '^8.6.0',
+      eslint: '7',
+      'eslint-config-masmott': 'file:./../../packages/eslint-config-masmott',
+      typescript: '4.4.4',
+    },
+    eslintConfig: {
+      extends: 'masmott',
+    },
+  };
+
+  return JSON.stringify(packageJsonContent);
+}
+
+export const tsconfig = {
+  compilerOptions: {
+    target: 'es5',
+    lib: ['dom', 'dom.iterable', 'esnext'],
+    allowJs: true,
+    skipLibCheck: true,
+    strict: true,
+    forceConsistentCasingInFileNames: true,
+    noEmit: true,
+    esModuleInterop: true,
+    module: 'esnext',
+    moduleResolution: 'node',
+    resolveJsonModule: true,
+    isolatedModules: true,
+    jsx: 'preserve',
+    noUncheckedIndexedAccess: true,
+    noPropertyAccessFromIndexSignature: true,
+    exactOptionalPropertyTypes: true,
+    noImplicitReturns: true,
+    noUnusedLocals: true,
+    noUnusedParameters: true,
+    noFallthroughCasesInSwitch: true,
+    sourceMap: true,
+    declaration: true,
+    declarationMap: true,
+    incremental: true,
+  },
+  include: ['next-env.d.ts', '**/*.ts', '**/*.tsx'],
+  exclude: ['node_modules'],
+};
