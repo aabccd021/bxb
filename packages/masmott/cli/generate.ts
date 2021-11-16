ef903a944a1cc0fbb4a1a65521620d8817d91a76
aabccd021 <aabccd021@gmail.com>
2021-11-16 09:30:21 +0700

    yaa

--- packages/masmott/cli/generate.ts
+++ packages/masmott/cli/generate.ts
@@ -3,21 +3,20 @@ import { isLeft } from 'fp-ts/lib/Either';
 import * as fs from 'fs';
 import { PathReporter } from 'io-ts/PathReporter';
 import { makeWriteFileActions, parseMasmottConfig } from './pure';
-import { WriteFileAction } from './types';
-
-function handleFileWriteAction({ dir, name, content }: WriteFileAction): void {
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, { recursive: true });
-  }
-  fs.writeFileSync(`${dir}/${name}`, content);
-}
 
 export function generate(): void {
   const configStr = fs.readFileSync('./masmott.yaml', { encoding: 'utf-8' });
-  const parseResult = parseMasmottConfig(configStr);
-  if (isLeft(parseResult)) {
-    throw Error(PathReporter.report(parseResult)[0]);
+  const configParseResult = parseMasmottConfig(configStr);
+  if (isLeft(configParseResult)) {
+    throw Error(PathReporter.report(configParseResult)[0]);
   }
-  makeWriteFileActions(parseResult.right).forEach(handleFileWriteAction);
+  const config = configParseResult.right;
+  const writeFileActions = makeWriteFileActions(config);
+  writeFileActions.forEach(({ dir, name, content }) => {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    fs.writeFileSync(`${dir}/${name}`, content);
+  });
   exec('yarn lint --fix');
 }
--- packages/masmott/server/view-join.ts
+++ packages/masmott/server/view-join.ts
@@ -76,66 +76,13 @@ async function getRefDocFromRefSpecs(
 }
 
 /**
- * Prefix a join name to a field name.
- *
- * @param joinName Name of the join view.
- * @param fieldName Field name to prefix.
- * @returns Field name prefixed by join name.
- */
-function prefixJoinName(joinName: string, fieldName: string): string {
-  const prefixedFieldName = `${joinName}_${fieldName}`;
-  return prefixedFieldName;
-}
-
-/**
- * Make refId field name of a join view.
- *
- * @param joinName Name of the join view.
- * @returns Name of refId field of the view.
- */
-function makeRefIdFieldName(joinName: string): string {
-  return prefixJoinName(joinName, 'id');
-}
-
-/**
- * Creates document data with fields name prefixed by join name.
- *
- * @param docData Document data to be process.
- * @param joinName Name of the join view
- * @returns Document data with prefixed fields name.
- */
-function prefixJoinNameOnDocData(docData: DocumentData, joinName: string): DocumentData {
-  return mapKeys(docData, (_, fieldName) => prefixJoinName(joinName, fieldName));
-}
-
-/**
  * Materialize join view data from a specification.
  *
  * @param srcDocData Source document data of the join view.
  * @param spec Materialization specification.
  * @returns Materialized join view data.
  */
-async function materializeJoinData(
-  srcDocData: DocumentData,
-  joinName: string,
-  spec: JoinSpec
-): Promise<DocumentData> {
-  const refDoc = await getRefDocFromRefSpecs(spec, srcDocData);
 
-  const selectedFieldDocData = pick(refDoc.data, spec.selectedFieldNames);
-  const compactDocData = compactObject(selectedFieldDocData);
-
-  const prefixedData = prefixJoinNameOnDocData(compactDocData, joinName);
-
-  const refIdFieldName = makeRefIdFieldName(joinName);
-
-  const docDataWithRefId = {
-    ...prefixedData,
-    [refIdFieldName]: refDoc.id,
-  };
-
-  return docDataWithRefId;
-}
 
 /**
  * Maps array of join view specification into array of materialized data.
