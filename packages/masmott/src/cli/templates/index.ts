7fdc84564b3017e053e119397a090ef301034e25
Muhamad Abdurahman <aabccd021@gmail.com>
2022-02-03 12:59:27 +0700

    generate tsconfig

--- packages/masmott/src/cli/templates/index.ts
+++ packages/masmott/src/cli/templates/index.ts
@@ -18,3 +18,41 @@ const nextConfig = {
 
 module.exports = nextConfig
 `;
+
+export const tsConfigJson = jsonStringify({
+  compilerOptions: {
+    allowJs: true,
+    alwaysStrict: true,
+    esModuleInterop: true,
+    exactOptionalPropertyTypes: true,
+    forceConsistentCasingInFileNames: true,
+    incremental: true,
+    isolatedModules: true,
+    jsx: 'preserve',
+    lib: ['dom', 'dom.iterable', 'esnext'],
+    module: 'esnext',
+    moduleResolution: 'node',
+    noEmit: true,
+    noFallthroughCasesInSwitch: true,
+    noImplicitAny: true,
+    noImplicitOverride: true,
+    noImplicitReturns: true,
+    noImplicitThis: true,
+    noPropertyAccessFromIndexSignature: true,
+    noUncheckedIndexedAccess: true,
+    noUnusedLocals: true,
+    noUnusedParameters: true,
+    resolveJsonModule: true,
+    skipLibCheck: true,
+    strict: true,
+    strictBindCallApply: true,
+    strictFunctionTypes: true,
+    strictNullChecks: true,
+    strictPropertyInitialization: true,
+    target: 'es5',
+    types: ['jest'],
+    useUnknownInCatchVariables: true,
+  },
+  exclude: ['node_modules'],
+  include: ['next-env.d.ts', '**/*.ts', '**/*.tsx'],
+});
