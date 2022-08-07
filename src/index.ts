a3ed9889d02ff97bc567ffa46b8ffedbd3dc8414
aabccd021 <aabccd021@gmail.com>
2022-08-07 15:09:57 +0700

    feat: implement materialize view with all properties of doc

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-export const helloWorld = () => 'hello world';
+import { pipe } from 'fp-ts/lib/function';
+import * as Record from 'fp-ts/Record';
+
+export type Field = { readonly relation: 'self' };
+export type View = {
+  readonly fields: Record<string, Field>;
+};
+export type TableViews = Record<string, View>;
+export type AppViews = Record<string, TableViews>;
+
+export type DocData = Record<string, unknown>;
+
+export type Doc = {
+  readonly table: string;
+  readonly view: string;
+  readonly id: string;
+  readonly data: DocData;
+};
+
+export type DB = {
+  readonly setDoc: (doc: Doc) => void;
+};
+
+export const makeTriggers = ({ views, db }: { readonly views: AppViews; readonly db: DB }) =>
+  pipe(
+    views,
+    Record.mapWithIndex((tableName, tableViews) => ({
+      onCreate: ({ id, data }: { readonly id: string; readonly data: DocData }) =>
+        pipe(
+          tableViews,
+          Record.mapWithIndex((viewName, _view) =>
+            db.setDoc({ id, data, table: tableName, view: viewName })
+          )
+        ),
+    }))
+  );
