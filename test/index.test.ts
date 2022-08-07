7e8574b8ab71b4f21f6b2c19a9e7dd7211b25ad7
aabccd021 <aabccd021@gmail.com>
2022-08-07 13:58:36 +0700

    test(index): add test for on create materialize
    refactor(index.test.ts): refactor imported function name
    
    test: fixed test assertion

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,52 @@
-import { describe, expect, it } from 'vitest';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { describe, expect, it, vi } from 'vitest';
 
-import { helloWorld } from '../src';
+import { makeTriggers } from '../src';
 
 describe.concurrent('masmott', () => {
-  it('can hello world', () => {
-    expect(helloWorld()).equals('hello world');
+  it('on create materialize view', () => {
+    const setDoc = vi
+      .fn()
+      .mockImplementation(
+        (_: {
+          readonly table: string;
+          readonly view: string;
+          readonly id: string;
+          readonly data: Record<string, unknown>;
+        }) => {}
+      );
+
+    const triggers = makeTriggers({
+      views: {
+        lawak: {
+          card: {
+            fields: {
+              text: {
+                relation: 'self',
+              },
+            },
+          },
+        },
+      },
+      db: {
+        setDoc,
+      },
+    });
+
+    triggers.lawak.onCreate({
+      id: 'fooLawak',
+      data: { text: 'lawak text' },
+    });
+
+    expect(setDoc).toHaveBeenCalledOnce();
+    expect(setDoc).toHaveBeenCalledWith({
+      table: 'lawak',
+      view: 'card',
+      id: 'fooLawak',
+      data: { text: 'lawak text' },
+    });
   });
 });
