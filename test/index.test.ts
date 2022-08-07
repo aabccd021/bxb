4e09448e99df9e7fcdeeb3137c6f8e7c17294789
aabccd021 <aabccd021@gmail.com>
2022-08-07 16:43:24 +0700

    refactor(test): remove unused ignores

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import * as E from 'fp-ts/Either';
 import * as TE from 'fp-ts/TaskEither';
 import { describe, expect, it } from 'vitest';
