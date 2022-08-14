73279d3ed2eb3b352711f2981ea5abdb1b933f73
aabccd021 <aabccd021@gmail.com>
2022-08-15 06:53:32 +0700

    feat(index): implement materialize onCreate

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/lib/function';
 import * as Record from 'fp-ts/Record';
+import * as T from 'fp-ts/Task';
 import * as TE from 'fp-ts/TaskEither';
 
 export type Field = { readonly relation: 'self' };
@@ -11,20 +13,32 @@ export type AppViews = Record<string, TableViews>;
 
 export type DocData = Record<string, unknown>;
 
-export type Doc = {
-  readonly table: string;
+export type DocKey = {
   readonly view: string;
   readonly id: string;
+  readonly table: string;
+};
+
+export type DocSnapshot = {
   readonly data: DocData;
+  readonly key: DocKey;
 };
 
 type DBGenerics = {
   readonly SetDocRight: unknown;
   readonly SetDocLeft: unknown;
+  readonly GetDocRight: unknown;
+  readonly GetDocLeft: unknown;
 };
 
 export type DB<U extends DBGenerics> = {
-  readonly setDoc: (doc: Doc) => TE.TaskEither<U['SetDocLeft'], U['SetDocRight']>;
+  readonly setDoc: (doc: DocSnapshot) => TE.TaskEither<U['SetDocLeft'], U['SetDocRight']>;
+  readonly getDoc: (
+    doc: DocKey
+  ) => TE.TaskEither<
+    U['GetDocLeft'],
+    { readonly data: DocData; readonly context: U['GetDocRight'] }
+  >;
 };
 
 export const makeTriggers = <U extends DBGenerics>({
@@ -37,12 +51,19 @@ export const makeTriggers = <U extends DBGenerics>({
   pipe(
     views,
     Record.mapWithIndex((tableName, tableViews) => ({
-      onCreate: ({ id, data }: { readonly id: string; readonly data: DocData }) =>
+      onCreate: ({
+        id,
+        data,
+      }: {
+        readonly id: string;
+        readonly data: DocData;
+      }): T.Task<Record<string, E.Either<U['SetDocLeft'], U['SetDocRight']>>> =>
         pipe(
           tableViews,
           Record.mapWithIndex((viewName, _view) =>
-            db.setDoc({ id, data, table: tableName, view: viewName })
-          )
+            db.setDoc({ key: { id, table: tableName, view: viewName }, data })
+          ),
+          Record.sequence(T.ApplicativePar)
         ),
     }))
   );
