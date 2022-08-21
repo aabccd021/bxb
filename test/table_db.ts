5a027554084a8446d78fb8d55b8f39d14c3501e5
aabccd021 <aabccd021@gmail.com>
2022-08-21 14:28:09 +0700

    test(server.test): init

---
+++ test/table_db.ts
@@ -0,0 +1,18 @@
+import * as E from 'fp-ts/Either';
+import { describe, expect, it } from 'vitest';
+
+describe.concurrent('Table DB', () => {
+  it('can set doc and get doc', async () => {
+    const createNoTriggerTableDB = createTableDB({});
+    const db = createNoTriggerTableDB();
+    const key = { table: 'sakurazaka', id: 'kira' };
+
+    const setDoc = db.setDoc({ key, data: { birthYear: 2002 } });
+    await setDoc();
+
+    const getDoc = db.getDoc({ key });
+    const result = await getDoc();
+
+    expect(result).toStrictEqual(E.right({ birthYear: 2002 }));
+  });
+});
