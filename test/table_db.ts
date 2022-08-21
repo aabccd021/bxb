4702b2e6b378a9bbaa7edffe5954486b869a562f
aabccd021 <aabccd021@gmail.com>
2022-08-21 14:49:22 +0700

    refactor(test/table.ts): refactor

--- test/table_db.ts
+++ test/table_db.ts
@@ -1,6 +1,8 @@
 import * as E from 'fp-ts/Either';
 import { describe, expect, it } from 'vitest';
 
+import { createTableDB } from '../src/server';
+
 describe.concurrent('Table DB', () => {
   it('can set doc and get doc', async () => {
     const createNoTriggerTableDB = createTableDB({});
@@ -10,7 +12,7 @@ describe.concurrent('Table DB', () => {
     const setDoc = db.setDoc({ key, data: { birthYear: 2002 } });
     await setDoc();
 
-    const getDoc = db.getDoc({ key });
+    const getDoc = db.getDoc(key);
     const result = await getDoc();
 
     expect(result).toStrictEqual(E.right({ birthYear: 2002 }));
