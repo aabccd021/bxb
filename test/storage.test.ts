3e6f0bf730777e241cd9ccc3fab589448a7fa10b
aabccd021 <aabccd021@gmail.com>
2022-08-25 05:32:03 +0700

    test(storage): add test for trigger on upload

--- test/storage.test.ts
+++ test/storage.test.ts
@@ -1,5 +1,10 @@
 import { Blob } from 'buffer';
+import * as Array from 'fp-ts/Array';
+import * as IO from 'fp-ts/IO';
+import * as IORef from 'fp-ts/IORef';
+import { pipe } from 'fp-ts/lib/function';
 import * as O from 'fp-ts/Option';
+import * as T from 'fp-ts/Task';
 import { describe, expect, it } from 'vitest';
 
 import { createStorage } from '../src/storage';
@@ -19,5 +24,21 @@ describe.concurrent('Storage', () => {
     const result = await download();
 
     expect(result).toStrictEqual(O.of(file));
+  });
+
+  it('can run trigger when object uploaded', async () => {
+    const logs = IORef.newIORef([])();
+    const createStorageWithTrigger = createStorage({
+      onUploaded: (id) => pipe(logs.read, IO.map(Array.append(id)), IO.chain(logs.write), T.fromIO),
+    });
+    const storage = createStorageWithTrigger();
+
+    const id = 'sakurazaka/kira';
+
+    const file = new Blob(['masumoto'], { type: 'text/plain' });
+    const upload = storage.upload({ id, file });
+    await upload();
+
+    expect(logs.read()).toStrictEqual(['sakurazaka/kira']);
   });
 });
