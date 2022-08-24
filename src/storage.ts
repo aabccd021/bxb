5252f5ea7a2fb6e785618c9c4ae67255fa65249d
aabccd021 <aabccd021@gmail.com>
2022-08-25 05:50:06 +0700

    feat(storage): implement trigger

--- src/storage.ts
+++ src/storage.ts
@@ -1,12 +1,14 @@
 import { Blob } from 'buffer';
 import * as IO from 'fp-ts/IO';
 import * as IORef from 'fp-ts/IORef';
-import { pipe } from 'fp-ts/lib/function';
+import { flow, pipe } from 'fp-ts/lib/function';
 import * as O from 'fp-ts/Option';
 import * as Record from 'fp-ts/Record';
 import * as T from 'fp-ts/Task';
 
-type StorageTriggers = {};
+type StorageTriggers = {
+  readonly onUploaded: (id: string) => T.Task<unknown>;
+};
 
 type StorageState = Record<string, Blob>;
 
@@ -15,15 +17,33 @@ type Storage = {
   readonly download: (id: string) => T.Task<O.Option<Blob>>;
 };
 
+const emptyTriggers: StorageTriggers = {
+  onUploaded: (_) => T.of(undefined),
+};
+
+const fillTriggersDefaults = (triggers: Partial<StorageTriggers>): StorageTriggers => ({
+  ...emptyTriggers,
+  ...triggers
+});
+
 const initialState: StorageState = {};
 
-export const createStorage = (_triggers: StorageTriggers): IO.IO<Storage> =>
-  pipe(
+const createStorageWithTriggers = (triggers: StorageTriggers): IO.IO<Storage> => {
+  return pipe(
     initialState,
     IORef.newIORef,
     IO.map((storage) => ({
       upload: ({ id, file }) =>
-        pipe(storage.read, IO.map(Record.upsertAt(id, file)), IO.chain(storage.write), T.fromIO),
+        pipe(
+          storage.read,
+          IO.map(Record.upsertAt(id, file)),
+          IO.chain(storage.write),
+          T.fromIO,
+          T.chain(() => triggers.onUploaded(id))
+        ),
       download: (id) => pipe(storage.read, IO.map(Record.lookup(id)), T.fromIO),
     }))
   );
+};
+
+export const createStorage = flow(fillTriggersDefaults, createStorageWithTriggers);
