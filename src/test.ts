5f45a5c62bcb8e81883daa3c879fdd34a45b83e5
aabccd021 <aabccd021@gmail.com>
2022-09-04 09:22:48 +0700

    refactor: fixed fp-ts imports

--- src/test.ts
+++ src/test.ts
@@ -1,11 +1,6 @@
 /* eslint-disable functional/no-expression-statement */
-import { option, task } from 'fp-ts';
+import { io, ioRef, option, readonlyArray, task } from 'fp-ts';
 import { flow, pipe } from 'fp-ts/function';
-import * as IO from 'fp-ts/IO';
-import * as IORef from 'fp-ts/IORef';
-import * as O from 'fp-ts/Option';
-import * as Array from 'fp-ts/ReadonlyArray';
-import * as T from 'fp-ts/Task';
 
 import { MakeClientWithConfig } from '../src';
 
@@ -14,16 +9,16 @@ export const getTextFromBlob =
   async () => {
     const donwloadResultBlob: Blob | undefined = pipe(
       downloadResult,
-      O.getOrElse<Blob | undefined>(() => undefined)
+      option.getOrElse<Blob | undefined>(() => undefined)
     );
     const downloadResultText = await donwloadResultBlob?.text();
-    return O.fromNullable(downloadResultText);
+    return option.fromNullable(downloadResultText);
   };
 
 export const stringToBlob = (text: string) => new Blob([text]);
 
 export type Test<Result> = {
-  readonly expect: T.Task<Result>;
+  readonly expect: task.Task<Result>;
   readonly toStrictEqual: Result;
 };
 
@@ -36,95 +31,110 @@ export const makeTest = (makeClientWithTrigger: MakeClientWithConfig): Tests => 
     'can upload and download': test({
       expect: pipe(
         makeClientWithTrigger({}),
-        T.bindTo('client'),
-        T.chainFirst(({ client }) =>
+        task.bindTo('client'),
+        task.chainFirst(({ client }) =>
           client.storage.upload({
             id: 'sakurazaka/kira',
             blob: stringToBlob('masumoto'),
           })
         ),
-        T.chain(({ client }) => client.storage.download('sakurazaka/kira')),
-        T.chain(getTextFromBlob)
+        task.chain(({ client }) => client.storage.download('sakurazaka/kira')),
+        task.chain(getTextFromBlob)
       ),
-      toStrictEqual: O.some('masumoto'),
+      toStrictEqual: option.some('masumoto'),
     }),
 
     'can run trigger when object uploaded': test({
       expect: pipe(
-        IORef.newIORef<readonly string[]>([]),
-        T.fromIO,
-        T.bindTo('logs'),
-        T.bind('client', ({ logs }) =>
+        ioRef.newIORef<readonly string[]>([]),
+        task.fromIO,
+        task.bindTo('logs'),
+        task.bind('client', ({ logs }) =>
           makeClientWithTrigger({
             storage: () => ({
               onUploaded: (id) =>
-                pipe(logs.read, IO.map(Array.append(id)), IO.chain(logs.write), T.fromIO),
+                pipe(
+                  logs.read,
+                  io.map(readonlyArray.append(id)),
+                  io.chain(logs.write),
+                  task.fromIO
+                ),
             }),
           })
         ),
-        T.chainFirst(({ client }) =>
+        task.chainFirst(({ client }) =>
           client.storage.upload({
             id: 'sakurazaka/kira',
             blob: stringToBlob('masumoto'),
           })
         ),
-        T.chain(({ logs }) => T.fromIO(logs.read))
+        task.chain(({ logs }) => task.fromIO(logs.read))
       ),
       toStrictEqual: ['sakurazaka/kira'],
     }),
 
     'still upload when having trigger': test({
       expect: pipe(
-        IORef.newIORef<readonly string[]>([]),
-        T.fromIO,
-        T.bindTo('logs'),
-        T.bind('client', ({ logs }) =>
+        ioRef.newIORef<readonly string[]>([]),
+        task.fromIO,
+        task.bindTo('logs'),
+        task.bind('client', ({ logs }) =>
           makeClientWithTrigger({
             storage: () => ({
               onUploaded: (id) =>
-                pipe(logs.read, IO.map(Array.append(id)), IO.chain(logs.write), T.fromIO),
+                pipe(
+                  logs.read,
+                  io.map(readonlyArray.append(id)),
+                  io.chain(logs.write),
+                  task.fromIO
+                ),
             }),
           })
         ),
-        T.chainFirst(({ client }) =>
+        task.chainFirst(({ client }) =>
           client.storage.upload({
             id: 'sakurazaka/kira',
             blob: stringToBlob('masumoto'),
           })
         ),
-        T.chain(({ client }) => client.storage.download('sakurazaka/kira')),
-        T.chain(getTextFromBlob)
+        task.chain(({ client }) => client.storage.download('sakurazaka/kira')),
+        task.chain(getTextFromBlob)
       ),
-      toStrictEqual: O.some('masumoto'),
+      toStrictEqual: option.some('masumoto'),
     }),
 
     'can download inside trigger': test({
       expect: pipe(
-        IORef.newIORef<readonly option.Option<string>[]>([]),
-        T.fromIO,
-        T.bindTo('logs'),
-        T.bind('client', ({ logs }) =>
+        ioRef.newIORef<readonly option.Option<string>[]>([]),
+        task.fromIO,
+        task.bindTo('logs'),
+        task.bind('client', ({ logs }) =>
           makeClientWithTrigger({
             storage: (storageAdmin) => ({
               onUploaded: flow(
                 storageAdmin.download,
-                T.chain(getTextFromBlob),
-                T.chain((text) =>
-                  pipe(logs.read, IO.map(Array.append(text)), IO.chain(logs.write), T.fromIO)
+                task.chain(getTextFromBlob),
+                task.chain((text) =>
+                  pipe(
+                    logs.read,
+                    io.map(readonlyArray.append(text)),
+                    io.chain(logs.write),
+                    task.fromIO
+                  )
                 )
               ),
             }),
           })
         ),
-        T.chainFirst(({ client }) =>
+        task.chainFirst(({ client }) =>
           client.storage.upload({
             id: 'sakurazaka/kira',
             blob: stringToBlob('masumoto'),
           })
         ),
-        T.chain(({ logs }) => T.fromIO(logs.read))
+        task.chain(({ logs }) => task.fromIO(logs.read))
       ),
-      toStrictEqual: [O.some('masumoto')],
+      toStrictEqual: [option.some('masumoto')],
     }),
   },
 
@@ -132,25 +142,25 @@ export const makeTest = (makeClientWithTrigger: MakeClientWithConfig): Tests => 
     'can set doc and get doc': test({
       expect: pipe(
         makeClientWithTrigger({}),
-        T.bindTo('client'),
-        T.chainFirst(({ client }) =>
+        task.bindTo('client'),
+        task.chainFirst(({ client }) =>
           client.db.setDoc({
             key: { table: 'sakurazaka', id: 'kira' },
             data: { birthYear: 2002 },
           })
         ),
-        T.chain(({ client }) => client.db.getDoc({ id: 'kira', table: 'sakurazaka' }))
+        task.chain(({ client }) => client.db.getDoc({ id: 'kira', table: 'sakurazaka' }))
       ),
-      toStrictEqual: O.of({ birthYear: 2002 }),
+      toStrictEqual: option.of({ birthYear: 2002 }),
     }),
 
     'returns empty option when getDoc non existing': test({
       expect: pipe(
         makeClientWithTrigger({}),
-        T.bindTo('client'),
-        T.chain(({ client }) => client.db.getDoc({ id: 'kira', table: 'sakurazaka' }))
+        task.bindTo('client'),
+        task.chain(({ client }) => client.db.getDoc({ id: 'kira', table: 'sakurazaka' }))
       ),
-      toStrictEqual: O.none,
+      toStrictEqual: option.none,
     }),
   },
 });
--- test/test.test.ts
+++ test/test.test.ts
@@ -1,5 +1,5 @@
+import { option } from 'fp-ts';
 import { pipe } from 'fp-ts/function';
-import * as O from 'fp-ts/Option';
 import { describe, expect, it } from 'vitest';
 
 import { getTextFromBlob, stringToBlob } from '../src/test';
@@ -7,15 +7,15 @@ import { getTextFromBlob, stringToBlob } from '../src/test';
 describe.concurrent('util', () => {
   describe.concurrent('stringBlob & getTextFromBlob', () => {
     it('blob content is its wrapped text', async () => {
-      const stringToBlobToText = pipe('masumoto', stringToBlob, O.of, getTextFromBlob);
+      const stringToBlobToText = pipe('masumoto', stringToBlob, option.of, getTextFromBlob);
       const result = await stringToBlobToText();
-      expect(result).toStrictEqual(O.some('masumoto'));
+      expect(result).toStrictEqual(option.some('masumoto'));
     });
 
     it('blob content is not equal to other than its wrapped text', async () => {
-      const stringToBlobToText = pipe('masumoto', stringToBlob, O.of, getTextFromBlob);
+      const stringToBlobToText = pipe('masumoto', stringToBlob, option.of, getTextFromBlob);
       const result = await stringToBlobToText();
-      expect(result).not.toStrictEqual(O.some('nazuna'));
+      expect(result).not.toStrictEqual(option.some('nazuna'));
     });
   });
 });
