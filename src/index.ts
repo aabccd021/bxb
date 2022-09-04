319b8a50be96b30e54d7afecb367bcffa571e562
aabccd021 <aabccd021@gmail.com>
2022-09-04 20:07:04 +0700

    refactor: remove io-ts and make-struct-ts

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,23 @@
 import { option, task } from 'fp-ts';
-import * as t from 'io-ts';
 
-const BlobFromUnknown = new t.Type<Blob, unknown, unknown>(
-  'BlobFromUnknown',
-  (u): u is Blob => u instanceof Blob,
-  (u, c) => (u instanceof Blob ? t.success(u) : t.failure(u, c)),
-  (a) => a
-);
-
-export const FileSnapshot = t.type({
-  id: t.string,
-  blob: BlobFromUnknown,
-});
-
-export type FileSnapshot = t.TypeOf<typeof FileSnapshot>;
+export type FileSnapshot = {
+  readonly id: string;
+  readonly blob: Blob;
+};
 
 export type TableDBTriggers = unknown;
 
-export const DocKey = t.type({
-  table: t.string,
-  id: t.string,
-});
+export type DocKey = {
+  readonly table: string;
+  readonly id: string;
+};
 
-export type DocKey = t.TypeOf<typeof DocKey>;
+export type DocData = Record<string, unknown>;
 
-export const DocData = t.UnknownRecord;
-
-export type DocData = t.TypeOf<typeof DocData>;
-
-export const DocSnapshot = t.type({
-  key: DocKey,
-  data: DocData,
-});
-
-export type DocSnapshot = t.TypeOf<typeof DocSnapshot>;
+export type DocSnapshot = {
+  readonly key: DocKey;
+  readonly data: DocData;
+};
 
 export type StorageAdmin = {
   readonly upload: (p: FileSnapshot) => task.Task<unknown>;
