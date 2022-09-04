bfd73b2d2b58588d76d7fbd9b89982740e4b959d
aabccd021 <aabccd021@gmail.com>
2022-09-04 08:24:07 +0700

    refactor(StorageAdmin.ts): refactor to use do notation

--- src/StorageAdmin.ts
+++ src/StorageAdmin.ts
@@ -1,36 +1,29 @@
-import { ioRef } from 'fp-ts';
+import { identity, ioRef } from 'fp-ts';
 import { pipe } from 'fp-ts/function';
 
-import {
-  Config,
-  ReadonlyStorageAdmin as ReadonlyStorageAdmin_,
-  StorageAdmin,
-  WriteonlyStorageAdmin as WriteonlyStorageAdmin_,
-} from '.';
+import { Config, StorageAdmin } from '.';
 import * as ReadonlyStorageAdmin from './ReadonlyStorageAdmin';
 import { StorageState } from './StorageState';
 import * as StorageTriggers from './StorageTriggers';
 import * as WriteonlyStorageAdmin from './WriteonlyStorageAdmin';
 
-const of3 =
-  (readonlyStorageAdmin: ReadonlyStorageAdmin_) =>
-  (writeonlyStorageAdmin: WriteonlyStorageAdmin_): StorageAdmin => ({
-    ...readonlyStorageAdmin,
-    ...writeonlyStorageAdmin,
-  });
-
-const of2 =
-  (storageState: ioRef.IORef<StorageState>, config: Required<Config>) =>
-  (readonlyStorageAdmin: ReadonlyStorageAdmin_): StorageAdmin =>
-    pipe(
-      readonlyStorageAdmin,
-      config.storage,
-      StorageTriggers.toRequired,
-      WriteonlyStorageAdmin.of(storageState),
-      of3(readonlyStorageAdmin)
-    );
-
 export const of =
   (config: Required<Config>) =>
   (storageState: ioRef.IORef<StorageState>): StorageAdmin =>
-    pipe(storageState, ReadonlyStorageAdmin.of, of2(storageState, config));
+    pipe(
+      storageState,
+      ReadonlyStorageAdmin.of,
+      identity.bindTo('readonlyStorageAdmin'),
+      identity.bind('writeonlyStorageAdmin', ({ readonlyStorageAdmin }) =>
+        pipe(
+          readonlyStorageAdmin,
+          config.storage,
+          StorageTriggers.toRequired,
+          WriteonlyStorageAdmin.of(storageState)
+        )
+      ),
+      identity.chain(({ readonlyStorageAdmin, writeonlyStorageAdmin }) => ({
+        ...readonlyStorageAdmin,
+        ...writeonlyStorageAdmin,
+      }))
+    );
