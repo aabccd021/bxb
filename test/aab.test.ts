05e6c51ee2cdc2b48a4330b06c5e56f4d241b928
aabccd021 <aabccd021@gmail.com>
2022-10-06 19:30:58 +0700

    refactor: ideal

--- test/aab.test.ts
+++ test/aab.test.ts
@@ -5,26 +5,15 @@ import { makeTagged } from '../src/union';
 
 const { summon } = summonFor({});
 
+// ideal
 export const Humanoid = makeTagged(summon)('type')({
-  Person: summon((F) =>
-    F.interface(
-      {
-        type: F.stringLiteral('Person'),
-        name: F.string(),
-        birthDate: F.number(),
-      },
-      'Person'
-    )
-  ),
-  Robot: summon((F) =>
-    F.interface(
-      {
-        type: F.stringLiteral('Robot'),
-        manufacturer: F.string(),
-      },
-      'Robot'
-    )
-  ),
+  Person: (F: any) => ({
+    name: F.string(),
+    birthDate: F.number(),
+  }),
+  Robot: (F: any) => ({
+    manufacturer: F.string(),
+  }),
 });
 
 test('a', () =>
