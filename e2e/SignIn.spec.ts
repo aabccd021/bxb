018dc53fdfdef05164a4fe9f98c811e7c4500c1d
aabccd021 <aabccd021@gmail.com>
2022-11-14 17:29:44 +0700

    test: codegen

--- e2e/SignIn.spec.ts
+++ e2e/SignIn.spec.ts
@@ -1,10 +1,22 @@
 /* eslint-disable functional/no-expression-statement */
 import { expect, test } from '@playwright/test';
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({
-  page,
-}) => {
-  await page.goto('/e2e/SignIn');
+test('test', async ({ page }) => {
+  await page.goto('/e2e/SignIn?');
 
-  await expect(page).toHaveTitle('Sign In');
+  await page.getByLabel('Email').click();
+
+  await page.getByLabel('Email').fill('a');
+
+  await page.getByLabel('Display name').click();
+
+  await page.getByLabel('Display name').fill('a');
+
+  await page.getByLabel('Profile photo URL').click();
+
+  await page.getByLabel('Profile photo URL').fill('b');
+
+  await page.getByRole('button', { name: 'Sign in with Provider' }).click();
+
+  await expect(page).toHaveURL('http://localhost:3000/e2e/SignIn?');
 });
