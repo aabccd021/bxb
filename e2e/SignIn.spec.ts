ebd4a33690afeefd4d9fc5986aad33498baccdb7
aabccd021 <aabccd021@gmail.com>
2022-11-15 12:26:03 +0700

    test: signInWithRedirect

--- e2e/SignIn.spec.ts
+++ e2e/SignIn.spec.ts
@@ -5,11 +5,11 @@ test('test', async ({ page }) => {
   await page.goto('/e2e/SignIn');
 
   await page.getByRole('button', { name: 'Sign In' }).click();
-  await expect(page).toHaveURL('/public/SignIn');
+  await expect(page).toHaveURL('/public/SignIn?redirectUrl=http%3A%2F%2Flocalhost%3A3000%2Fe2e%2FSignIn');
 
   await page.getByLabel('Email').click();
   await page.getByLabel('Email').fill('aab');
+  await page.getByRole('button', { name: 'Sign in' }).click();
 
-  await page.getByRole('button', { name: 'Sign in with Provider' }).click();
-  await expect(page).toHaveURL('/public/SignIn?');
+  await expect(page).toHaveURL('/e2e/SignIn');
 });
--- src/mock.ts
+++ src/mock.ts
@@ -2,6 +2,8 @@
 /* eslint-disable functional/immutable-data */
 /* eslint-disable functional/no-return-void */
 export const signIn = () => {
-  console.log('aabccd');
-  window.location.href = '/public/SignIn';
+  console.log(window.location);
+  const url = new URL(`${window.location.origin}/public/SignIn`);
+  url.searchParams.append('redirectUrl', window.location.href);
+  window.location.href = url.toString();
 };
