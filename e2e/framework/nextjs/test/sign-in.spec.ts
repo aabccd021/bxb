d77aa1e55e55233460aea0641fcf3c80e3d5d91b
aabccd021 <aabccd021@gmail.com>
2022-11-16 16:33:39 +0700

    build: onAuthStatusChanged

--- e2e/framework/nextjs/test/sign-in.spec.ts
+++ e2e/framework/nextjs/test/sign-in.spec.ts
@@ -4,12 +4,13 @@ import { expect, test } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('/');
 
+  await expect(page.locator('#auth-status')).toHaveText('not signed in');
   await page.getByRole('button', { name: 'Sign In' }).click();
   await expect(page).toHaveURL('/__masmott__/signIn?redirectUrl=http%3A%2F%2Flocalhost%3A3000%2F');
 
-  await page.getByLabel('Email').click();
   await page.getByLabel('Email').fill('aab');
   await page.getByRole('button', { name: 'Sign in' }).click();
 
   await expect(page).toHaveURL('/');
+  await expect(page.locator('#auth-status')).toHaveText('email : aab');
 });
