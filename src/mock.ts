20c6f27c3fba8999d6864456cf0a80e9d3672205
aabccd021 <aabccd021@gmail.com>
2022-11-16 16:44:52 +0700

    build: object

--- src/mock.ts
+++ src/mock.ts
@@ -2,14 +2,19 @@ import { Unsubscribe } from './type';
 /* eslint-disable functional/no-expression-statement */
 /* eslint-disable functional/immutable-data */
 /* eslint-disable functional/no-return-void */
-export const signIn = () => {
+const signIn = () => {
   const url = new URL(`${window.location.origin}/__masmott__/signIn`);
   url.searchParams.append('redirectUrl', window.location.href);
   window.location.href = url.toString();
 };
 
-export const onAuthStateChanged = (callback: (user?: string) => void): Unsubscribe => {
+const onAuthStateChanged = (callback: (user?: string) => void): Unsubscribe => {
   const user = localStorage.getItem('auth') ?? undefined;
   callback(user);
   return () => undefined;
+};
+
+export const mock = {
+  signIn,
+  onAuthStateChanged,
 };
