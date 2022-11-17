dc3486a00feb0883c69f28a794c11ede5a978eab
aabccd021 <aabccd021@gmail.com>
2022-11-17 16:46:17 +0700

    refactor: simplify hook

--- e2e/framework/nextjs/pages/index.tsx
+++ e2e/framework/nextjs/pages/index.tsx
@@ -1,26 +1,34 @@
-/* eslint-disable functional/no-conditional-statement */
 /* eslint-disable functional/no-expression-statement */
 /* eslint-disable functional/no-return-void */
 
 import { option } from 'fp-ts';
 import { Option } from 'fp-ts/Option';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import { masmott } from '../masmott';
 
-export default function Home() {
-  const [auth, setAuth] = useState<Option<string>>(option.none);
+const mapToAuthStatus = option.match(
+  () => 'not signed in',
+  (s) => `email : ${s}`
+);
+
+const useHome = () => {
+  const [authStatus, setAuth] = useState<Option<string>>(option.none);
   useEffect(() => masmott.onAuthStateChanged((a) => () => setAuth(a))(), [setAuth]);
+  const authStatusStr = useMemo(() => mapToAuthStatus(authStatus), [authStatus]);
+  return {
+    signInWithRedirect: masmott.signInGoogleWithRedirect,
+    signOut: masmott.signOut,
+    authStatusStr,
+  };
+};
+
+export default function Home() {
+  const { signInWithRedirect, signOut, authStatusStr } = useHome();
   return (
     <div>
-      <button onClick={() => masmott.signInGoogleWithRedirect()}>Sign In With Redirect</button>;
-      <button onClick={() => masmott.signOut()}>Sign Out</button>;
-      <p id="auth-status">
-        {option.match(
-          () => 'not signed in',
-          (s) => `email : ${s}`
-        )(auth)}
-      </p>
+      <button onClick={signInWithRedirect}>Sign In With Redirect</button>;
+      <button onClick={signOut}>Sign Out</button>;<p id="auth-status">{authStatusStr}</p>
     </div>
   );
 }
