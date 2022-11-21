ac0378e3eb2938699902de174a1d1704132920f9
aabccd021 <aabccd021@gmail.com>
2022-11-21 13:12:37 +0700

    build: swc provider foo

--- e2e/framework/nextjs/test/treeshaking.sh
+++ e2e/framework/nextjs/test/treeshaking.sh
@@ -2,7 +2,7 @@
 
 lines=$(pnpm build | grep treeshaking | sed 's/([^)]*)//g' | tr -s ' ')
 echo "$lines"
-if [[ "$lines" != *"/treeshaking/sign-in 466 B 79.2 kB"* ]]; then
+if [[ "$lines" != *"/treeshaking/sign-in 467 B 79.2 kB"* ]]; then
   exit 1
 fi
 if [[ "$lines" != *"/treeshaking/sign-out 426 B 79.5 kB"* ]]; then
