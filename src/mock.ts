733f7bd251adcd9c3f038189cae5ddd1c3926187
aabccd021 <aabccd021@gmail.com>
2022-11-22 11:26:12 +0700

    Revert "test: red"
    This reverts commit 6bd505ecf1aae2210aaec5c50e1ff3c87cb21b35.

--- src/mock.ts
+++ src/mock.ts
@@ -1,66 +1,60 @@
-import { either, io, ioOption, ioRef, option, task, taskEither } from 'fp-ts';
+import { io, ioOption, ioRef, option, task, taskEither } from 'fp-ts';
 import { pipe } from 'fp-ts/function';
 import { IO } from 'fp-ts/IO';
 import { Option } from 'fp-ts/Option';
 
 import { mkFpWindow } from './mkFp';
-import { FpWindow, GetDownloadUrlError, OnAuthStateChangedCallback, Stack } from './type';
+import { FpWindow, OnAuthStateChangedCallback, Stack } from './type';
 
 const mkRedirectUrl = ({ origin, href }: { readonly origin: string; readonly href: string }) => {
   const searchParamsStr = new URLSearchParams({ redirectUrl: href }).toString();
   return `${origin}/__masmott__/signInWithRedirect?${searchParamsStr}`;
 };
 
-const signInWithRedirect = (win: FpWindow) =>
+const signInWithRedirect = (window: FpWindow) =>
   pipe(
     io.Do,
-    io.bind('origin', () => win.location.origin),
-    io.bind('href', () => win.location.href.get),
+    io.bind('origin', () => window.location.origin),
+    io.bind('href', () => window.location.href.get),
     io.map(mkRedirectUrl),
-    io.chain(win.location.href.set)
+    io.chain(window.location.href.set)
   );
 
-export const mkStackFromWindow = (mkWindow: IO<typeof window>): IO<Stack> =>
-  pipe(
+export const mkStackFromDom = (mkWindow: IO<typeof window>): IO<Stack> => {
+  const window = mkFpWindow(mkWindow);
+  return pipe(
     io.Do,
     io.bind('onAuthStateChangedCallback', () =>
       ioRef.newIORef<Option<OnAuthStateChangedCallback>>(option.none)
     ),
-    io.bind('win', () => io.map(mkFpWindow)(mkWindow)),
-    io.map(({ onAuthStateChangedCallback, win }) => ({
+    io.map(({ onAuthStateChangedCallback }) => ({
       ci: {
         deployStorage: () => task.of(undefined),
         deployDb: () => task.of(undefined),
       },
       client: {
         storage: {
-          uploadBase64: ({ key, file }) =>
-            task.fromIO(win.localStorage.setItem(`storage/${key}`, file)),
-          getDownloadUrl: ({ key }) =>
-            pipe(
-              win.localStorage.getItem(`storage/${key}`),
-              io.map(either.fromOption(() => GetDownloadUrlError.Union.of.FileNotFound({}))),
-              taskEither.fromIOEither
-            ),
+          uploadString: () => task.of(undefined),
+          getDownloadUrl: () => taskEither.of(''),
         },
         db: {
           setDoc: () => task.of(undefined),
           getDoc: () => taskEither.of({}),
         },
         auth: {
-          signInWithGoogleRedirect: signInWithRedirect(win),
+          signInWithGoogleRedirect: signInWithRedirect(window),
           createUserAndSignInWithEmailAndPassword: (email, _password) =>
             pipe(
               io.Do,
               io.chain(() => onAuthStateChangedCallback.read),
               ioOption.chainFirstIOK((onChangedCallback) => onChangedCallback(option.some(email))),
-              io.chainFirst(() => win.localStorage.setItem('auth', email))
+              io.chainFirst(() => window.localStorage.setItem('auth', email))
             ),
           onAuthStateChanged: (onChangedCallback) =>
             pipe(
               io.Do,
               io.chainFirst(() => onAuthStateChangedCallback.write(option.some(onChangedCallback))),
-              io.bind('lsAuth', () => win.localStorage.getItem('auth')),
+              io.bind('lsAuth', () => window.localStorage.getItem('auth')),
               io.chainFirst(({ lsAuth }) => onChangedCallback(lsAuth)),
               io.map(() => onAuthStateChangedCallback.write(option.none))
             ),
@@ -68,9 +62,10 @@ export const mkStackFromWindow = (mkWindow: IO<typeof window>): IO<Stack> =>
             io.Do,
             io.chain(() => onAuthStateChangedCallback.read),
             ioOption.chainFirstIOK((onChangedCallback) => onChangedCallback(option.none)),
-            io.chainFirst(() => win.localStorage.removeItem('auth'))
+            io.chainFirst(() => window.localStorage.removeItem('auth'))
           ),
         },
       },
     }))
   );
+};
