9ad6f8886def8f3dd30fdf9c3f599d6e1930adf7
aabccd021 <aabccd021@gmail.com>
2022-11-22 11:39:52 +0700

    Revert "Revert "test: red""
    This reverts commit abd1c7f8620234037a87f81ad8f9b6f70bdc67b8.

--- src/mkFp.ts
+++ src/mkFp.ts
@@ -1,45 +1,21 @@
 import { io, option } from 'fp-ts';
 import { pipe } from 'fp-ts/function';
-import { IO } from 'fp-ts/IO';
 
 import { FpWindow } from './type';
 
-export const mkFpWindow = (mkDom: IO<typeof window>): FpWindow => ({
+export const mkFpWindow = (win: typeof window): FpWindow => ({
   location: {
-    origin: pipe(
-      mkDom,
-      io.map((window) => window.location.origin)
-    ),
+    origin: () => win.location.origin,
     href: {
-      get: pipe(
-        mkDom,
-        io.map((window) => window.location.href)
-      ),
-      set: (newHref) =>
-        pipe(
-          mkDom,
-          io.chain((window) => () => {
-            window.location.href = newHref;
-          })
-        ),
+      get: () => win.location.href,
+      set: (newHref) => () => {
+        win.location.href = newHref;
+      },
     },
   },
   localStorage: {
-    getItem: (key) =>
-      pipe(
-        mkDom,
-        io.chain((window) => () => window.localStorage.getItem(key)),
-        io.map(option.fromNullable)
-      ),
-    setItem: (key, value) =>
-      pipe(
-        mkDom,
-        io.chain((window) => () => window.localStorage.setItem(key, value))
-      ),
-    removeItem: (key) =>
-      pipe(
-        mkDom,
-        io.chain((window) => () => window.localStorage.removeItem(key))
-      ),
+    getItem: (key) => pipe(() => win.localStorage.getItem(key), io.map(option.fromNullable)),
+    setItem: (key, value) => () => win.localStorage.setItem(key, value),
+    removeItem: (key) => () => win.localStorage.removeItem(key),
   },
 });
