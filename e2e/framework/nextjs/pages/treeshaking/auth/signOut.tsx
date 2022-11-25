d0f21b661f86ef52554cfebdebd210f926773fc9
aabccd021 <aabccd021@gmail.com>
2022-11-25 10:16:58 +0700

    test: more treeshaking test

---
+++ e2e/framework/nextjs/pages/treeshaking/auth/signOut.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable functional/no-expression-statement */
+/* eslint-disable functional/no-return-void */
+
+import { masmott } from '../../../masmott';
+
+export default function Home() {
+  masmott.auth.signInWithGoogleRedirect;
+  return <div> </div>;
+}
---
+++ e2e/framework/nextjs/pages/treeshaking/db/getDoc.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable functional/no-expression-statement */
+/* eslint-disable functional/no-return-void */
+
+import { masmott } from '../../../masmott';
+
+export default function Home() {
+  masmott.db.getDoc;
+  return <div> </div>;
+}
