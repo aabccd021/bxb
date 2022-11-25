9af527971ac7e0bcc5149118d71b613faea368ad
aabccd021 <aabccd021@gmail.com>
2022-11-25 10:55:03 +0700

    refactor: /next

--- src/next.ts
+++ src/next.ts
@@ -1,30 +1,28 @@
 import { NextConfig } from 'next';
 import { PHASE_DEVELOPMENT_SERVER } from 'next/constants';
 
-const rewrites = (phase: string) =>
-  phase === PHASE_DEVELOPMENT_SERVER
-    ? [
-        {
-          source: '/__masmott__/signInWithRedirect',
-          destination: '/masmott/signInWithRedirect.html',
-        },
-      ]
-    : [];
-
 export const withMasmott =
   (conf: NextConfig) =>
   (phase: string): NextConfig => ({
     ...conf,
     rewrites: async () => {
-      const rew = await conf.rewrites?.();
-      const rews = rewrites(phase);
-      return rew === undefined
-        ? rews
-        : Array.isArray(rew)
-        ? [...rew, ...rews]
+      const oldRewrites = await conf.rewrites?.();
+      const newRewrites =
+        phase === PHASE_DEVELOPMENT_SERVER
+          ? [
+              {
+                source: '/__masmott__/signInWithRedirect',
+                destination: '/masmott/signInWithRedirect.html',
+              },
+            ]
+          : [];
+      return oldRewrites === undefined
+        ? newRewrites
+        : Array.isArray(oldRewrites)
+        ? [...oldRewrites, ...newRewrites]
         : {
-            ...rew,
-            beforeFiles: [...rew.beforeFiles, ...rews],
+            ...oldRewrites,
+            beforeFiles: [...oldRewrites.beforeFiles, ...newRewrites],
           };
     },
   });
