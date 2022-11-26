f5f628f391bc743fc78c6eb8a9782cc77650d0fa
aabccd021 <aabccd021@gmail.com>
2022-11-27 05:30:49 +0700

    Revert "feat: image Upload nextjs"
    This reverts commit 4e06c92594196b2dcfd7e3491071eaf7b9b03651.

--- e2e/framework/nextjs/pages/index.tsx
+++ e2e/framework/nextjs/pages/index.tsx
@@ -1,8 +1,6 @@
-import { either, option, taskEither} from 'fp-ts';
-import { pipe } from 'fp-ts/lib/function';
+import { option } from 'fp-ts';
 import { Option } from 'fp-ts/lib/Option';
-import { TaskEither } from 'fp-ts/lib/TaskEither';
-import { ChangeEvent, useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import { masmott } from '../masmott';
 
@@ -10,17 +8,6 @@ const mapToAuthStatus = option.match(
   () => 'not signed in',
   (s) => `email : ${s}`
 );
-
-const readAsDataUrl = (file: File): TaskEither<string, string| ArrayBuffer| null> => taskEither.tryCatch(() => {
-  return new Promise((resolve, reject) => {
-    var fr = new FileReader();  
-    fr.onload = () => {
-      resolve(fr.result)
-    };
-    fr.onerror = reject;
-    fr.readAsDataURL(file);
-  });
-}, JSON.stringify)
 
 const useAuthState = () => {
   const [authState, setAuthState] = useState<Option<string>>(option.none);
@@ -39,36 +26,13 @@ const useHome = () => {
   };
 };
 
-const uploadAndDownload =  (c: ChangeEvent<HTMLInputElement>) =>       
-  pipe(
-    option.fromNullable(c.target.files?.[0]), 
-    taskEither.fromOption(() => 'file not selected'), 
-    taskEither.chainW(readAsDataUrl), 
-    taskEither.chainEitherKW(either.fromPredicate((dataUrl): dataUrl is string => typeof dataUrl === 'string', () => 'dataUrl is not a string')),
-    taskEither.chainW((dataUrl) => masmott.storage.uploadDataUrl({key: 'aabccd021', dataUrl})),
-    taskEither.chainW(() => masmott.storage.getDownloadUrl({key: 'aabccd021'})),
-  )
-
-
 export default function Home() {
-  const [downloadUrl, setDownloadUrl] = useState<string>();
   const { authStateStr } = useHome();
-  useEffect(() => {
-    pipe(
-    masmott.storage.getDownloadUrl({key: 'aabccd021'}),
-    taskEither.chainIOK((downloadUrl) => () => setDownloadUrl(downloadUrl))
-    )()
-  },[])
   return (
     <div>
       <button onClick={masmott.auth.signInWithGoogleRedirect}>Sign In With Redirect</button>
       <button onClick={masmott.auth.signOut}>Sign Out</button>
       <p id="auth-status">{authStateStr}</p>
-      {downloadUrl !== undefined ? <img src={downloadUrl}/> : <div>non</div>}
-      <input type='file' onChange={(c) => pipe(
-        uploadAndDownload(c),
-        taskEither.chainIOK((downloadUrl) => () => setDownloadUrl(downloadUrl))
-      )()}/>
     </div>
   );
 }
