7c7d38bac9a818fc38a7905276df6eb8b36760f0
aabccd021 <aabccd021@gmail.com>
2022-12-09 13:00:04 +0700

    refactor: test

--- src/test/stack/client/auth/onAuthStateChanged.ts
+++ src/test/stack/client/auth/onAuthStateChanged.ts
@@ -22,28 +22,6 @@ export const tests = [
   }),
 
   defineTest({
-    name: 'auth state changes to signed in after sign in',
-    expect: ({ client }) =>
-      pipe(
-        taskEither.fromIO(ioRef.newIORef<AuthState>(option.none)),
-        taskEither.chainW((authStateRef) =>
-          pipe(
-            taskEither.fromIO(client.auth.onAuthStateChanged(authStateRef.write)),
-            taskEither.chainW(() =>
-              client.auth.createUserAndSignInWithEmailAndPassword({
-                email: 'kira@sakurazaka.com',
-                password: 'dorokatsu',
-              })
-            ),
-            taskEither.chainW(() => taskEither.fromIO(authStateRef.read))
-          )
-        ),
-        taskEither.map(option.isSome)
-      ),
-    toResult: either.right(true),
-  }),
-
-  defineTest({
     name: 'auth state changes to signed in after sign in when subscribed',
     expect: ({ client }) =>
       pipe(
@@ -66,28 +44,6 @@ export const tests = [
   }),
 
   defineTest({
-    name: 'auth state changes to signed out after sign in and then sign out',
-    expect: ({ client }) =>
-      pipe(
-        taskEither.fromIO(ioRef.newIORef<AuthState>(option.none)),
-        taskEither.chainW((authStateRef) =>
-          pipe(
-            taskEither.fromIO(client.auth.onAuthStateChanged(authStateRef.write)),
-            taskEither.chainW(() =>
-              client.auth.createUserAndSignInWithEmailAndPassword({
-                email: 'kira@sakurazaka.com',
-                password: 'dorokatsu',
-              })
-            ),
-            taskEither.chainW(() => client.auth.signOut),
-            taskEither.chainW(() => taskEither.fromIO(authStateRef.read))
-          )
-        )
-      ),
-    toResult: either.right(option.none),
-  }),
-
-  defineTest({
     name: 'auth state changes to signed out after sign in and then sign out and subscribe',
     expect: ({ client }) =>
       pipe(
@@ -101,50 +57,6 @@ export const tests = [
             taskEither.chainW(() => client.auth.signOut),
             taskEither.chainW(() =>
               taskEither.fromIO(client.auth.onAuthStateChanged(authStateRef.write))
-            ),
-            taskEither.chainW(() => taskEither.fromIO(authStateRef.read))
-          )
-        )
-      ),
-    toResult: either.right(option.none),
-  }),
-
-  defineTest({
-    name: `auth state changes to signed out after sign in and then sign out when subscribed in between`,
-    expect: ({ client }) =>
-      pipe(
-        taskEither.fromIO(ioRef.newIORef<AuthState>(option.none)),
-        taskEither.chainW((authStateRef) =>
-          pipe(
-            client.auth.createUserAndSignInWithEmailAndPassword({
-              email: 'kira@sakurazaka.com',
-              password: 'dorokatsu',
-            }),
-            taskEither.chainW(() =>
-              taskEither.fromIO(client.auth.onAuthStateChanged(authStateRef.write))
-            ),
-            taskEither.chainW(() => client.auth.signOut),
-            taskEither.chainW(() => taskEither.fromIO(authStateRef.read))
-          )
-        )
-      ),
-    toResult: either.right(option.none),
-  }),
-
-  defineTest({
-    name: 'auth state does not change after unsubscribed',
-    expect: ({ client }) =>
-      pipe(
-        taskEither.fromIO(ioRef.newIORef<AuthState>(option.none)),
-        taskEither.chainW((authStateRef) =>
-          pipe(
-            taskEither.fromIO(client.auth.onAuthStateChanged(authStateRef.write)),
-            taskEither.chainW((unsubscribe) => taskEither.fromIO(unsubscribe)),
-            taskEither.chainW(() =>
-              client.auth.createUserAndSignInWithEmailAndPassword({
-                email: 'kira@sakurazaka.com',
-                password: 'dorokatsu',
-              })
             ),
             taskEither.chainW(() => taskEither.fromIO(authStateRef.read))
           )
--- src/test/stack/client/auth/signOut.ts
+++ src/test/stack/client/auth/signOut.ts
@@ -31,28 +31,6 @@ export const tests = [
   }),
 
   defineTest({
-    name: 'auth state changes to signed out after sign in and then sign out and subscribe',
-    expect: ({ client }) =>
-      pipe(
-        taskEither.fromIO(ioRef.newIORef<AuthState>(option.none)),
-        taskEither.chainW((authStateRef) =>
-          pipe(
-            client.auth.createUserAndSignInWithEmailAndPassword({
-              email: 'kira@sakurazaka.com',
-              password: 'dorokatsu',
-            }),
-            taskEither.chainW(() => client.auth.signOut),
-            taskEither.chainW(() =>
-              taskEither.fromIO(client.auth.onAuthStateChanged(authStateRef.write))
-            ),
-            taskEither.chainW(() => taskEither.fromIO(authStateRef.read))
-          )
-        )
-      ),
-    toResult: either.right(option.none),
-  }),
-
-  defineTest({
     name: `auth state changes to signed out after sign in and then sign out when subscribed in between`,
     expect: ({ client }) =>
       pipe(
