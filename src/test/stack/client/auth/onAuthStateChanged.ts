2e9fc670d6a05fdf8078eb0faa1c54cc2ab097ae
aabccd021 <aabccd021@gmail.com>
2022-12-09 13:35:01 +0700

    refactor: rename and restructure test

--- src/test/stack/client/auth/onAuthStateChanged.ts
+++ src/test/stack/client/auth/onAuthStateChanged.ts
@@ -6,7 +6,7 @@ import { defineTest } from '../../../util';
 
 export const tests = [
   defineTest({
-    name: 'initial auth state is signed out',
+    name: 'returns signed out as default auth state',
     expect: ({ client }) =>
       pipe(
         ioRef.newIORef<AuthState>(option.none),
@@ -22,7 +22,7 @@ export const tests = [
   }),
 
   defineTest({
-    name: 'auth state changes to signed in after sign in when subscribed',
+    name: 'returns singed in state after client.auth.createUserAndSignInWithEmailAndPassword',
     expect: ({ client }) =>
       pipe(
         taskEither.fromIO(ioRef.newIORef<AuthState>(option.none)),
@@ -44,7 +44,7 @@ export const tests = [
   }),
 
   defineTest({
-    name: 'auth state changes to signed out after sign in and then sign out and subscribe',
+    name: `returns singed out state after client.auth.createUserAndSignInWithEmailAndPassword then client.auth.signOut then subscribe`,
     expect: ({ client }) =>
       pipe(
         taskEither.fromIO(ioRef.newIORef<AuthState>(option.none)),
@@ -57,6 +57,72 @@ export const tests = [
             taskEither.chainW(() => client.auth.signOut),
             taskEither.chainW(() =>
               taskEither.fromIO(client.auth.onAuthStateChanged(authStateRef.write))
+            ),
+            taskEither.chainW(() => taskEither.fromIO(authStateRef.read))
+          )
+        )
+      ),
+    toResult: either.right(option.none),
+  }),
+
+  defineTest({
+    name: `returns singed out state after subscribe and client.auth.createUserAndSignInWithEmailAndPassword then client.auth.signOut`,
+    expect: ({ client }) =>
+      pipe(
+        taskEither.fromIO(ioRef.newIORef<AuthState>(option.none)),
+        taskEither.chainW((authStateRef) =>
+          pipe(
+            taskEither.fromIO(client.auth.onAuthStateChanged(authStateRef.write)),
+            taskEither.chainW(() =>
+              client.auth.createUserAndSignInWithEmailAndPassword({
+                email: 'kira@sakurazaka.com',
+                password: 'dorokatsu',
+              })
+            ),
+            taskEither.chainW(() => client.auth.signOut),
+            taskEither.chainW(() => taskEither.fromIO(authStateRef.read))
+          )
+        )
+      ),
+    toResult: either.right(option.none),
+  }),
+
+  defineTest({
+    name: `returns singed out state after client.auth.createUserAndSignInWithEmailAndPassword then subscribe then client.auth.signOut`,
+    expect: ({ client }) =>
+      pipe(
+        taskEither.fromIO(ioRef.newIORef<AuthState>(option.none)),
+        taskEither.chainW((authStateRef) =>
+          pipe(
+            client.auth.createUserAndSignInWithEmailAndPassword({
+              email: 'kira@sakurazaka.com',
+              password: 'dorokatsu',
+            }),
+            taskEither.chainW(() =>
+              taskEither.fromIO(client.auth.onAuthStateChanged(authStateRef.write))
+            ),
+            taskEither.chainW(() => client.auth.signOut),
+            taskEither.chainW(() => taskEither.fromIO(authStateRef.read))
+          )
+        )
+      ),
+    toResult: either.right(option.none),
+  }),
+
+  defineTest({
+    name: 'does not call onAuthStateChanged callback after unsubscribed',
+    expect: ({ client }) =>
+      pipe(
+        taskEither.fromIO(ioRef.newIORef<AuthState>(option.none)),
+        taskEither.chainW((authStateRef) =>
+          pipe(
+            taskEither.fromIO(client.auth.onAuthStateChanged(authStateRef.write)),
+            taskEither.chainW((unsubscribe) => taskEither.fromIO(unsubscribe)),
+            taskEither.chainW(() =>
+              client.auth.createUserAndSignInWithEmailAndPassword({
+                email: 'kira@sakurazaka.com',
+                password: 'dorokatsu',
+              })
             ),
             taskEither.chainW(() => taskEither.fromIO(authStateRef.read))
           )
