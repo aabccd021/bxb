afcf214c64d78d8948038d3979d27a0a410614b5
aabccd021 <aabccd021@gmail.com>
2022-12-15 16:19:35 +0700

    test: UploadDataUrlAwaitFunctions.ts

--- src/type/client/storage/index.ts
+++ src/type/client/storage/index.ts
@@ -1,7 +1,9 @@
 import * as GetDownloadUrl from './GetDownloadUrl';
 import * as UploadDataUrl from './UploadDataUrl';
+import * as UploadDataUrlAwaitFunctions from './UploadDataUrlAwaitFunctions';
 export type Scope = {
+  readonly uploadDataUrlAwaitFunctions: UploadDataUrlAwaitFunctions.Fn;
   readonly uploadDataUrl: UploadDataUrl.Fn;
   readonly getDownloadUrl: GetDownloadUrl.Fn;
 };
-export { GetDownloadUrl, UploadDataUrl };
+export { GetDownloadUrl, UploadDataUrl, UploadDataUrlAwaitFunctions };
