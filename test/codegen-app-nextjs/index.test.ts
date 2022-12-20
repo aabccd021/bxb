441b1468f6e5c8af8879abc98d5f0ddec3c74960
aabccd021 <aabccd021@gmail.com>
2022-12-20 17:14:46 +0700

    refactor: compress

--- test/codegen-app-nextjs/index.test.ts
+++ test/codegen-app-nextjs/index.test.ts
@@ -5,37 +5,25 @@ import { test } from 'vitest';
 const exec = util.promisify(cp.exec);
 
 test('stack-foo can install packages', async () => {
-  await exec('pnpm install', {
-    cwd: `${__dirname}/packages/bxb-stack-foo`,
-    encoding: 'utf8',
-  });
+  await exec('pnpm install', { cwd: `${__dirname}/packages/bxb-stack-foo`, encoding: 'utf8' });
 });
 
 test(
   'stack-foo can build packages',
   async () => {
-    await exec('pnpm build', {
-      cwd: `${__dirname}/packages/bxb-stack-foo`,
-      encoding: 'utf8',
-    });
+    await exec('pnpm build', { cwd: `${__dirname}/packages/bxb-stack-foo`, encoding: 'utf8' });
   },
   { timeout: 20000 }
 );
 
 test('stack-bar can install packages', async () => {
-  await exec('pnpm install', {
-    cwd: `${__dirname}/packages/bxb-stack-bar`,
-    encoding: 'utf8',
-  });
+  await exec('pnpm install', { cwd: `${__dirname}/packages/bxb-stack-bar`, encoding: 'utf8' });
 });
 
 test(
   'stack-bar can build packages',
   async () => {
-    await exec('pnpm build', {
-      cwd: `${__dirname}/packages/bxb-stack-bar`,
-      encoding: 'utf8',
-    });
+    await exec('pnpm build', { cwd: `${__dirname}/packages/bxb-stack-bar`, encoding: 'utf8' });
   },
   { timeout: 20000 }
 );
@@ -43,7 +31,7 @@ test(
 test(
   'can generate for nextjs app',
   async () => {
-    await exec('pnpm ts-node-esm ./scripts/bxb.ts generate nextjs', {
+    await exec('pnpm ts-node ./scripts/bxb.ts generate nextjs', {
       cwd: `${__dirname}/packages/app-nextjs`,
       encoding: 'utf8',
     });
@@ -54,10 +42,7 @@ test(
 test(
   'can build nextjs app',
   async () => {
-    await exec('pnpm next build', {
-      cwd: `${__dirname}/packages/app-nextjs`,
-      encoding: 'utf8',
-    });
+    await exec('pnpm next build', { cwd: `${__dirname}/packages/app-nextjs`, encoding: 'utf8' });
   },
   { timeout: 30000 }
 );
