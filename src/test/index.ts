1f3e37c26fde9690cee56efa6c5604e9795cd9c8
aabccd021 <aabccd021@gmail.com>
2022-12-22 21:25:50 +0700

    refactor: compilable

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,15 +1,14 @@
-import { readonlyArray, readonlyRecord, taskEither } from 'fp-ts';
-import { pipe } from 'fp-ts/function';
+import { boolean, readonlyArray, readonlyRecord, string, taskEither } from 'fp-ts';
+import { identity, pipe } from 'fp-ts/function';
 import type { IO } from 'fp-ts/IO';
 import type { Task } from 'fp-ts/Task';
 import type { TaskEither } from 'fp-ts/TaskEither';
-import * as t from 'io-ts';
 import { match } from 'ts-pattern';
 import { expect, test as test_ } from 'vitest';
 
 import * as functions from './functions';
 import * as stackTests from './stack';
-import type { AnyStack, MakeCapabilitiesFilter, MakeStackFilter, MayCapability, PartialStack, StackFilter, StackFromFilter, Test } from './util';
+import type { AnyFilter, AnyStack, StackFilter, StackSet, Test } from './util';
 import { flattenTests } from './util';
 
 const runSimpleTest = <T>({
@@ -37,62 +36,40 @@ const runSimpleTest = <T>({
       runner(name, () => expect(task()).resolves.toEqual(result), { timeout, retry })
   );
 
-type RunTest = <S extends StackFilter>(p: {
-  readonly stack: TaskEither<unknown, StackFromFilter<S>>;
-}) => (test: Test) => IO<void>;
+// eslint-disable-next-line @typescript-eslint/ban-types
+type RunTest = (getStack: TaskEither<unknown, AnyStack>) => (test: Test<{}>) => IO<void>;
 
-export const runTest: RunTest =
-  ({ stack }) =>
-  (test) =>
-    runSimpleTest({ ...test, expect: pipe(stack, taskEither.chainW(test.expect)) });
+export const runTest: RunTest = (getStack) => (test) =>
+  runSimpleTest({
+    ...test,
+    expect: pipe(
+      getStack,
+      taskEither.chainW((k) => test.expect(k))
+    ),
+  });
 
-export const isUnknownReadonlyRecord = t.record(t.string, t.unknown).is;
-
-
-const stackToFilter = <T extends AnyStack>(stack: T): MakeStackFilter<T> =>
+const stackToFilter = (stack: StackSet): StackFilter =>
   pipe(
     stack,
-    readonlyRecord.map(capToFilter)
-    // either.fromPredicate(isUnknownReadonlyRecord, identity),
-    // either.matchW(
-    //   () => true as MakeStackFilter<T>,
-    //   (x) => stackToFilter(x) as MakeStackFilter<T>
-    // )
+    readonlyRecord.map((stackOrCapability) =>
+      typeof stackOrCapability === 'function' ? true : stackToFilter(stackOrCapability)
+    )
   );
 
-// const isSubRecord = ({
-//   stack,
-//   filter,
-// }: {
-//   readonly stack: ReadonlyRecord<string, unknown>;
-//   readonly filter: ReadonlyRecord<string, unknown>;
-// }): boolean =>
-//   pipe(
-//     filter,
-//     readonlyRecord.mapWithIndex((subName, filterSub) =>
-//       pipe(
-//         stack,
-//         readonlyRecord.lookup(subName),
-//         option.map(
-//           flow(
-//             either.fromPredicate(isUnknownReadonlyRecord, identity),
-//             either.match(
-//               () => true,
-//               (stackSub) => true
-//             )
-//           )
-//         ),
-//         option.getOrElse(() => false)
-//       )
-//     ),
-//     readonlyRecord.foldMap(string.Ord)(boolean.MonoidAll)(identity)
-//   );
+const isSubRecord = (a: AnyFilter, b: AnyFilter): boolean =>
+  pipe(
+    b,
+    readonlyRecord.mapWithIndex((k, bb: AnyFilter | boolean) =>
+      pipe(a[k], (aa) =>
+        typeof aa === 'object' && typeof bb === 'object' ? isSubRecord(aa, bb) : aa === bb
+      )
+    ),
+    readonlyRecord.foldMap(string.Ord)(boolean.MonoidAll)(identity)
+  );
 
-export const getTestsByStack = ({ stack }: { readonly stack: PartialStack }) => {
-  const a = stackToFilter(stack);
+export const getTestsByStack = (stack: AnyStack) =>
   pipe(
     { functions, stackTests },
     flattenTests,
-    readonlyArray.filter((test) => isSubRecord({ stack, filter: test.stack }))
+    readonlyArray.filter((test) => isSubRecord(test.stack, stackToFilter(stack)))
   );
-};
