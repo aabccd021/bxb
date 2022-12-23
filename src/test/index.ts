07a512f2fd8eee5b93d7bb988def24b798d6ad9c
aabccd021 <aabccd021@gmail.com>
2022-12-23 16:29:50 +0700

    refactor: unit

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,44 +1,13 @@
 import { boolean, readonlyArray, readonlyRecord, string, taskEither } from 'fp-ts';
 import { identity, pipe } from 'fp-ts/function';
 import type { IO } from 'fp-ts/IO';
-import type { Task } from 'fp-ts/Task';
 import type { TaskEither } from 'fp-ts/TaskEither';
-import { match } from 'ts-pattern';
-import { expect, test as test_ } from 'vitest';
 
+import { runSimpleTest } from '../simple-test';
 import * as functions from './functions';
 import * as stackTests from './stack';
 import type { AnyFilter, AnyStack, CapabilitySet, StackFilter, Test } from './util';
 import { flattenTests } from './util';
-
-type SimpleTest<T> = {
-  readonly name: string;
-  readonly expect: Task<T>;
-  readonly type?: 'fail';
-  readonly toResult: T;
-  readonly timeout?: number;
-  readonly retry?: number;
-};
-
-export const runSimpleTest = <T>({
-  type,
-  name,
-  timeout,
-  retry,
-  expect: task,
-  toResult: result,
-}: SimpleTest<T>): IO<void> =>
-  pipe(
-    match(type)
-      .with('fail', () => test_.fails)
-      .with(undefined, () => test_)
-      .exhaustive(),
-    // eslint-disable-next-line functional/no-return-void
-    (runner) => () =>
-      runner(name, () => expect(task()).resolves.toEqual(result), { timeout, retry })
-  );
-
-export const simpleTest = <T>(t: SimpleTest<T>) => t;
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type RunTest = (getStack: TaskEither<unknown, AnyStack>) => (test: Test<{}>) => IO<void>;
