b8d469d16222963f8f2a25455b4ddc4b50a44f3a
aabccd021 <aabccd021@gmail.com>
2022-12-26 12:52:32 +0700

    fix: run-script

--- src/simple-test.ts
+++ src/simple-test.ts
@@ -1,9 +1,13 @@
-import { deepStrictEqual } from 'assert';
-import { console, either, readonlyArray, readonlyRecord, task, taskEither } from 'fp-ts';
-import { apply, flow, pipe } from 'fp-ts/function';
+import { deepStrictEqual } from 'node:assert';
+import * as fs from 'node:fs/promises';
+
+import { console, either, readonlyArray, readonlyRecord, semigroup, task, taskEither } from 'fp-ts';
+import { flow, pipe } from 'fp-ts/function';
+import type { ReadonlyRecord } from 'fp-ts/ReadonlyRecord';
 import type { Task } from 'fp-ts/Task';
 import type { TaskEither } from 'fp-ts/TaskEither';
 import { withTimeout } from 'fp-ts-contrib/lib/Task/withTimeout';
+import printDiff from 'print-diff';
 import * as retry from 'retry-ts';
 import { retrying } from 'retry-ts/lib/Task';
 import { match } from 'ts-pattern';
@@ -54,51 +58,49 @@ const runAssertion = <T>(test: {
   pipe(
     taskEither.fromIO(console.log(`Start ${test.name}`)),
     taskEither.chainTaskK(() => test.expect),
+    taskEither.chainFirstIOK((result) => () => {
+      printDiff(JSON.stringify(result, undefined, 2), JSON.stringify(test.toResult, undefined, 2));
+      printDiff.unified(
+        JSON.stringify(result, undefined, 2),
+        JSON.stringify(test.toResult, undefined, 2)
+      );
+      printDiff.inline(
+        JSON.stringify(result, undefined, 2),
+        JSON.stringify(test.toResult, undefined, 2)
+      );
+    }),
     taskEither.chainEitherK((result) =>
       // eslint-disable-next-line functional/no-return-void
       either.tryCatch(() => deepStrictEqual(result, test.toResult), either.toError)
     ),
-    taskEither.mapLeft(readonlyArray.of),
     taskEither.chainFirstIOK(() => console.log(`Finished ${test.name}`)),
-    withTimeout(either.left(['timed out'] as readonly unknown[]), test.timeout ?? 5000)
+    withTimeout(either.left('timed out' as unknown), test.timeout ?? 5000),
+    taskEither.mapLeft((err) => ({ [test.name]: err }))
   );
 
-const runTestWithoutRetry = (test: Test) => (): TaskEither<readonly unknown[], unknown> =>
-  match(test)
-    .with({ type: 'single' }, runAssertion)
-    .with({ type: 'sequential' }, (t) =>
-      pipe(
-        t.tests,
-        readonlyRecord.mapWithIndex((subtestName, subTest) => ({
-          ...subTest,
-          name: `${t.name} > ${subtestName}`,
-        })),
-        readonlyRecord.traverse(taskEither.ApplicativeSeq)(runAssertion)
+const runTestWithoutRetry =
+  (test: Test) => (): TaskEither<ReadonlyRecord<string, unknown>, unknown> =>
+    match(test)
+      .with({ type: 'single' }, runAssertion)
+      .with({ type: 'sequential' }, (t) =>
+        pipe(
+          t.tests,
+          readonlyRecord.mapWithIndex((subtestName, subTest) => ({
+            ...subTest,
+            name: `${t.name} > ${subtestName}`,
+          })),
+          readonlyRecord.traverse(taskEither.ApplicativeSeq)(runAssertion)
+        )
       )
-    )
-    .exhaustive();
+      .exhaustive();
 
 const getRetryPolicy = (test: Test) =>
   match(test)
     .with({ type: 'single', retry: { type: 'policy' } }, (t) => t.retry.value)
     .with({ type: 'single', retry: { type: 'count' } }, (t) => retry.limitRetries(t.retry.value))
-    .with({ type: 'single', retry: undefined }, () => retry.limitRetries(0))
+    .with({ type: 'single' }, () => retry.limitRetries(0))
     .with({ type: 'sequential' }, () => retry.limitRetries(0))
     .exhaustive();
-
-export const runParallelWithConcurrency =
-  (concurrency: number) =>
-  <A>(tasks: readonly Task<A>[]): Task<readonly A[]> =>
-    pipe(
-      () => import('p-limit'),
-      task.map(
-        flow(
-          (pLimit) => pLimit.default(concurrency),
-          (limiter) => readonlyArray.traverse(task.ApplicativePar)((t: Task<A>) => () => limiter(t))
-        )
-      ),
-      task.chain(apply(tasks))
-    );
 
 export const runParallel = readonlyArray.sequence(task.ApplicativePar);
 
@@ -114,21 +116,25 @@ export const runTest = (test: Test) =>
   retrying(getRetryPolicy(test), runTestWithoutRetry(test), either.isLeft);
 
 export const aggregateErrors = task.map(
-  readonlyArray.sequence(either.getApplicativeValidation(readonlyArray.getSemigroup<unknown>()))
+  readonlyArray.sequence(
+    either.getApplicativeValidation(readonlyRecord.getUnionSemigroup(semigroup.last<unknown>()))
+  )
 );
 
 export const logAndThrowOnError = flow(
   taskEither.swap,
-  taskEither.chainIOK(console.error),
-  taskEither.chainIOK(() => setExitCode(1))
+  taskEither.chainFirstIOK(console.log),
+  taskEither.chainFirstTaskK(
+    (res) => () => fs.writeFile('aab.json', JSON.stringify(res, undefined, 2))
+  ),
+  taskEither.chainFirstIOK(() => setExitCode(1))
 );
 
 export const getTestTasks = readonlyArray.map(runTest);
 
 export const runTests = flow(getTestTasks, runParallel, aggregateErrors, logAndThrowOnError);
 
-export const runTestsWithMaxConcurrency = (concurrency: number) =>
-  flow(getTestTasks, runParallelWithConcurrency(concurrency), aggregateErrors, logAndThrowOnError);
+export const runTestsSeq = flow(getTestTasks, runSequential, aggregateErrors, logAndThrowOnError);
 
 export const simpleTest = <T>(t: Omit<SingleTest<T>, 'type'>): SingleTest<T> => ({
   ...t,
