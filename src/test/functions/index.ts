6d240801ff1c5b2cc80e3a960ae12290a7b4b64d
aabccd021 <aabccd021@gmail.com>
2023-01-17 15:50:24 +0700

    fix: test

--- src/test/functions/index.ts
+++ src/test/functions/index.ts
@@ -1,6 +1,5 @@
-import type { ScopeTests } from '../util';
 import { exportScopeTests } from '../util';
 import * as onAuthUserCreated from './onAuthUserCreated';
 import * as onObjectCreated from './onObjectCreated';
 
-export const tests = exportScopeTests({ onAuthUserCreated, onObjectCreated } as ScopeTests);
+export const tests = exportScopeTests({ onAuthUserCreated, onObjectCreated });
--- src/test/index.ts
+++ src/test/index.ts
@@ -1,39 +1,11 @@
 import { boolean, readonlyArray, readonlyRecord, string, taskEither } from 'fp-ts';
 import { flow, identity, pipe } from 'fp-ts/function';
 import type { TaskEither } from 'fp-ts/TaskEither';
-import { match } from 'ts-pattern';
 
-import type * as simple from '../simple-test';
 import * as functions from './functions';
 import * as stackTests from './stack';
 import type { AnyFilter, AnyStack, CapabilitySet, StackFilter, Test } from './util';
 import { flattenTests } from './util';
-
-export const runTestsWithRunner =
-  (getStack: TaskEither<unknown, AnyStack>) =>
-  (tests: readonly Test[]): readonly simple.Test[] =>
-    pipe(
-      tests,
-      readonlyArray.map(
-        (test): simple.Test =>
-          match(test)
-            .with({ type: 'single' }, (singleTest) => ({
-              ...singleTest,
-              expect: pipe(getStack, taskEither.chainW(singleTest.expect)),
-            }))
-            .with({ type: 'sequential' }, (sequentialTests) => ({
-              ...sequentialTests,
-              tests: pipe(
-                sequentialTests.tests,
-                readonlyRecord.map((sequentialTest) => ({
-                  ...sequentialTest,
-                  expect: pipe(getStack, taskEither.chainW(sequentialTest.expect)),
-                }))
-              ),
-            }))
-            .exhaustive()
-      )
-    );
 
 const stackToFilter = (stack: CapabilitySet): StackFilter =>
   pipe(
