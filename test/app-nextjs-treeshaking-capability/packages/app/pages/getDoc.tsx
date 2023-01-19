97b1a291b65123a895922a90c35ed53cc5a01f2f
aabccd021 <aabccd021@gmail.com>
2023-01-19 19:22:19 +0700

    fix: should be green

--- test/app-nextjs-treeshaking-capability/packages/app/pages/getDoc.tsx
+++ test/app-nextjs-treeshaking-capability/packages/app/pages/getDoc.tsx
@@ -3,7 +3,7 @@ import { bxb } from '../modules/bxb';
 export default function Home() {
   return (
     <div>
-      <button onClick={bxb.db.getDoc({key: {collection: 'a', id: 'b'}})}>Get Doc</button>
+      <button onClick={bxb.db.getDoc({ key: { collection: 'a', id: 'b' } })}>Get Doc</button>
     </div>
   );
 }
--- test/app-nextjs-treeshaking-capability/packages/app/pages/upsertDoc.tsx
+++ test/app-nextjs-treeshaking-capability/packages/app/pages/upsertDoc.tsx
@@ -3,7 +3,9 @@ import { bxb } from '../modules/bxb';
 export default function Home() {
   return (
     <div>
-      <button onClick={bxb.db.upsertDoc({key: {collection: 'a', id: 'b'}, data: {}})}>Get Doc</button>
+      <button onClick={bxb.db.upsertDoc({ key: { collection: 'a', id: 'b' }, data: {} })}>
+        Get Doc
+      </button>
     </div>
   );
 }
